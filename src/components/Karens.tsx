b8c376cdbf1a4366a8e82c357ba5a5fa6ebfa4c5
acodesmith <adam@acodesmith.com>
2020-07-08 20:46:10 -0400

    better mobile Karen and footer

--- src/components/Karens.tsx
+++ src/components/Karens.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { Karen, KarenProps } from "./Karen";
 import './karens.scss'
 
-const sortRandom = () => {
-	return 0.5 - Math.random()
-}
-
 export const Karens: React.FC<{ data: KarenProps[] }> = ({ data = [] }) => (
 	<div className="karens-wrapper">
-		{data.sort(sortRandom).map((karen: KarenProps) => (
+		{data.map((karen: KarenProps) => (
 			<Karen key={karen.name} {...karen} />
 		))}
 	</div>
