876f5d880756be63761017deec26fdee6c863d4c
acodesmith <adam@acodesmith.com>
2020-07-08 20:57:43 -0400

    Adding AK icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import './footer.scss';
 export const Footer: React.FC = () => (
 	<footer className="main">
 		<span>© {new Date().getFullYear()}, ACODESMITH</span>
-		<span className="pl-2">Site By Karen Hodge &amp; <a href="https://acodesmith.com">Adam Smith</a>
+		<span className="pl-2">Site  By Karen Hodge &amp; <a href="https://acodesmith.com">Adam Smith</a>
 		<a className="rounded bg-blue-900 text-blue-100 px-2 py-1 mx-2 text-xs" href="https://www.buymeacoffee.com/acodesmith">Buy Us a Beer 🍺!</a>
 		</span>
 	</footer>
